1fa1900560351b609c5fa631fbcdfa16cb9acf36
Nathan Sharfi <bonusfrogs@gmail.com>
2013-05-08 19:05:01 -0700

    add Civil Disobedience; generate nearly all the things
    markdown-file changes:
    - remove metadata in markdown files (it's in .json now)
    
    preprocessing changes:
    - generate Dublin Core metadata from .json (generate-dcmetadata.py)
    - move the WorkInfo object to its own module
    - generate iTunesMetadata.plist from .json in orthodoxize.py
    - don't remove the title pages anymore
    - use jankyjson.py to get metadata from .json to pandoc
    - both of these are now in the "Quikscript" genre
    
    æsthetics:
    - have a nice horizontal line between the author/title/date info and the rest of the work in HTML pages
    - don't include Alegreya anymore

---
+++ jankyjson.py
@@ -0,0 +1,35 @@
+# coding: UTF-8
+
+"""
+usage:
+jankyjson.py [file] [selector]
+
+"""
+
+import sys
+import json
+import re
+
+ARRAY_ACCESS = re.compile(r"([\w-]+)\[(\d+)\]")
+
+
+if len(sys.argv) != 3:
+    sys.stderr.write(__doc__+'\n')
+    sys.exit(1)
+
+fn = sys.argv[1]
+selector = sys.argv[2]
+r = u''
+
+with open(fn) as f:
+    j = json.load(f)
+    try:
+        m = ARRAY_ACCESS.match(selector)
+        if m:
+            r = j[m.group(1)][int(m.group(2))]
+        else:
+            r = j[selector]
+    except KeyError as e:
+        sys.stderr.write(u"couldn’t find key “{}” in file “{}”\n".format(selector, fn))
+
+    sys.stdout.write(r.encode('UTF-8'))
--- orthodoxize.py
+++ orthodoxize.py
@@ -7,30 +7,8 @@ from shutil import copyfile
 import sys
 import json
 import codecs
-
-
-
-class WorkInfo(object):
-    def __init__(self, prefix):
-        self.prefix = prefix
-        
-        self.directory = self.prefix + '.d'
-        self.navpath = join(self.directory, 'nav.xhtml')
-        self.opfpath = join(self.directory, 'content.opf')
-        self.titlepagepath = join(self.directory, 'title_page.xhtml')
-        self.itmfilename = self.prefix + ".iTunesMetadata.plist"
-        self.itmpath = join(self.directory, "iTunesMetadata.plist")
-        self.metadata = {}
-
-        try:
-            with open(self.prefix + '.json') as f:
-                metadata = json.load(f)
-                if metadata:
-                    self.metadata = metadata
-        except IOError as e:
-            print "I/O error({}): {}".format(e.errno, e.strerror)
-                
-        self.titles = self.metadata.get('titles', [])
+import plistlib
+from workinfo import WorkInfo
         
 
 
@@ -80,30 +58,31 @@ def remove_title_page(wi):
     opf.write(wi.opfpath)
 
 
-def generate_dc_metadata(wi):
-    ss = []
-    ins =  "author  year rights".split()
-    outs = "creator date rights".split()
-    
-    for i, o in zip(ins, outs):
-        ov = wi.metadata.get(i)
-        if ov:
-            ss.append(u"<dc:{}>{}</dc:{}>".format(o, ov, o))
 
-    contributors = wi.metadata.get('contributors', [])
-    for contributor in contributors:
-        ss.append(u"<dc:contributor>{}</dc:contributor>".format(contributor))  
-    
-    s = u'\n'.join(ss)
-    with codecs.open(wi.prefix + ".dcmetadata.xml", "w") as f:
-        f.write(s)
       
 
+def generate_plist(wi):
+    d = {
+        'artistName': wi.metadata['author'],
+        'sort-artist': wi.metadata['author-sort'],
+        'sort-artist-status': 1,
 
+        'releaseDate': unicode(wi.metadata['year']),
+        'year': unicode(wi.metadata['year']),
+
+        'genre': wi.metadata['genre'],
+        
+        'itemName': wi.metadata['titles'][0],
+        'playlistName': wi.metadata['titles'][0],
+        
+        'book-info': {'mime-type':"application/epub+zip"}            
+    }
+    
+    plistlib.writePlist(d, wi.itmpath)
 
 def add_itunes_metadata(wi):
     
-    copyfile(wi.itmfilename, wi.itmpath)
+    generate_plist(wi)
     
     ET.register_namespace('', "http://www.idpf.org/2007/opf")
     ET.register_namespace('dc', "http://purl.org/dc/elements/1.1/")
@@ -128,6 +107,6 @@ for basename in sys.argv[1:]:
     wi = WorkInfo(basename)
     
     fix_nav_file(wi)
-    remove_title_page(wi)
+#    remove_title_page(wi)
     generate_dc_metadata(wi)
     add_itunes_metadata(wi)
